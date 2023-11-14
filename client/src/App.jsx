2cef91a0c7640b57afa4155fd8857b1f890f40ce
surendrareddygaddam <your.email@example.com>
2023-11-14 13:38:47 +0530

    create header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,8 +4,10 @@ import Singin from './pages/Signin'
 import Signup from './pages/Signup'
 import Profile from './pages/Profile'
 import About from './pages/About'
+import Header from './componennts/Header'
 export default function App() {
   return <BrowserRouter>
+  <Header/>
   <Routes>
     <Route path='/' element={<Home/>}/>
     <Route path='/about' element={<About/>}/>
---
+++ client/src/componennts/Header.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom"
+
+export default function Header() {
+  return (
+    <div className='bg-slate-200'>
+        <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+            <Link to='/'><h1 className="font-bold">Auth App</h1></Link>
+            <ul className="flex gap-4">
+                <Link to='/'><li>Home</li></Link>
+                <Link to='/about'>About</Link>
+                <Link to='/sing-in'><li>Sign In</li></Link>
+            </ul>
+        </div>
+    </div>
+  )
+}
