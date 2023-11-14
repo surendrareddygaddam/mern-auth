599943142469829de256287cd39ad3b4b10ddf45
surendrareddygaddam <your.email@example.com>
2023-11-14 15:03:43 +0530

    create user model

--- api/index.js
+++ api/index.js
@@ -6,6 +6,9 @@ mongoose.connect(process.env.MONGO)
 .then(()=>{
     console.log(`Connected to MongoDB...`)
 })
+.catch((err)=>{
+    console.log(err)
+})
 const app = express()
 
 app.listen(3000,()=>{
